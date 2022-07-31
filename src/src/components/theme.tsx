9902cef7a86dc0acf894e5033f6e7afe41509a2b
Ang Jun Ray <angjunray@live.com>
2022-07-31 10:58:25 +0800

    basics of main page done

--- src/src/components/theme.tsx
+++ src/src/components/theme.tsx
@@ -20,7 +20,7 @@ export const themeOptions: ThemeOptions = {
     },
   },
   typography: {
-    fontFamily: 'IBM Plex Sans',
+    fontFamily: 'Helvetica',
   },
   spacing: 8,
 };
--- src/src/pages/home/home.tsx
+++ src/src/pages/home/home.tsx
@@ -1,9 +1,35 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
 
 export default function Home() {
-	return (
-		<div id="home">
-			<Typography variant="h6">Welcome to...</Typography>
-		</div>
-	);
+    return (
+        <Stack id="home" style={{ padding: '1vw' }} direction="column">
+            <Typography
+                variant="h6"
+                fontWeight={510}
+                sx={{ color: 'primary.light' }}
+            >
+                Welcome to...
+            </Typography>
+            <Stack id="logo" direction="row">
+                <motion.img
+                    src="/images/chess_set/version2/white_pawn.png"
+                    style={{ width: 'auto', height: 90 }}
+                    animate={{ rotate: [0, 90, 180, 270, 360] }}
+                    transition={{
+                        repeat: Infinity,
+                        repeatType: 'loop',
+                        repeatDelay: 0.5,
+                    }}
+                />
+                <Typography
+                    variant="h1"
+                    fontWeight={510}
+                    sx={{ color: 'secondary.light' }}
+                >
+                    AJR's Chess Arena
+                </Typography>
+            </Stack>
+        </Stack>
+    );
 }
